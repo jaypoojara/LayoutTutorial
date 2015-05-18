c302813d7682e4c4e1364cf775f377406ec7c27b
jay.poojara <jaypoojara007@gmail.com>
2015-05-18 13:19:41 +0530

    LayoutTutorial

---
+++ src/com/tutorial/layouttutorial/LinearLayoutTutorial.java
@@ -0,0 +1,13 @@
+package com.tutorial.layouttutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinearLayoutTutorial extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_linear_layout_tutorial);
+	}
+}
---
+++ src/com/tutorial/layouttutorial/RelativeLayout.java
@@ -0,0 +1,13 @@
+package com.tutorial.layouttutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeLayout extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_relative_layout);
+	}
+}
